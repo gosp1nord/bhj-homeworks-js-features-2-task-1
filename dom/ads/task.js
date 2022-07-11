608733eca3352466c234e71462e15184945fa200
gosp1nord <64732383+gosp1nord@users.noreply.github.com>
2022-07-11 13:31:04 +0300

    added file

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,13 +1,31 @@
 
 
-const divReveal = document.querySelectorAll('.reveal');
 
-document.addEventListener('scroll', () => {
-    divReveal.forEach((item) => {
-        let top = item.getBoundingClientRect().top;
-        if (top < window.innerHeight) {
-            item.classList.add('reveal_active');
+ function rotating() {
+    return new Promise(resolve => {
+        let elemAds = document.querySelector('.rotator__case_active');
+        let speed = Number(elemAds.nextElementSibling.getAttribute('data-speed'));
+        let nextElem;
+        if (elemAds.nextElementSibling === document.querySelector('.rotator').lastElementChild) {
+            nextElem = document.querySelector('.rotator').firstElementChild;
+        } else {
+            nextElem = elemAds.nextElementSibling;
         }
+        let color = nextElem.getAttribute('data-color');
+        setTimeout(function () {
+            elemAds.classList.toggle('rotator__case_active');
+            nextElem.classList.toggle('rotator__case_active');
+            nextElem.style.color = color;
+            resolve(true)
+        }, speed)
     })
+}
 
-})
+
+async function testAds() {
+    while (true) {
+        await rotating();
+    }
+}
+testAds();
+
