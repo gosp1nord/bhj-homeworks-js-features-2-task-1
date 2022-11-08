aa4a9ebdc3b42d72564ecd2b448bb281bc59229b
gosp1nord <64732383+gosp1nord@users.noreply.github.com>
2022-11-08 16:36:25 +0300

    add file

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,29 @@
+
+
+const linkA = document.querySelectorAll('a');
+linkA.forEach((item) => {
+    const title = item.title;
+    const blockTitle = document.createElement('div');
+    blockTitle.setAttribute('class', 'tooltip');
+    blockTitle.innerText = title;
+    item.insertAdjacentElement('afterend', blockTitle);
+
+    item.addEventListener('click', addTooltip);
+})
+
+
+function addTooltip(event) {
+    event.preventDefault();
+    const linkA = document.querySelectorAll('a');
+    linkA.forEach((item) => {
+        if (item === this) {
+            let nextBlock = item.nextElementSibling
+            nextBlock.classList.add('tooltip_active');
+            nextBlock.style.left = item.offsetLeft + 'px';
+        } else {
+            item.nextElementSibling.classList.remove('tooltip_active');
+        }
+    });
+
+}
+
